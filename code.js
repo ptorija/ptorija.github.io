e39125a86c9bbb0c5e85bedd67d7a9490c4273e6
ptorija <pablotorija23@gmail.com>
2024-01-21 17:45:58 +0100

    slider done

--- code.js
+++ code.js
@@ -1,21 +1,25 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+document.addEventListener('DOMContentLoaded', function () {
 
-window.onscroll = () => {
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('header nav a');
+
+  window.onscroll = () => {
     sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-        if(top >= offset && top < offset + height) {
-            // Remove 'active' class from all navigation links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-            });
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+      if(top >= offset && top < offset + height) {
+        // Remove 'active' class from all navigation links
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+        });
 
-            // Add 'active' class to the current navigation link
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            console.log('Active Section: ', id);
-        };
+        // Add 'active' class to the current navigation link
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        console.log('Active Section: ', id);
+      };
     });
-};
+  }; 
+
+});
